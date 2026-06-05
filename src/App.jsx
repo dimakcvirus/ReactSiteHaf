5a14eb66f2ec00af3536a33dee109913a37ef37f
dimakc <dimakc@mail.ru>
2026-06-05 15:51:53 +0300

    add balconiesLoggias page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { HafMain } from './HafMain/HafMain';
 import { Company } from './Company/Company';
 import { PvcWindows } from './ProductsPage/PvcWindows/PvcWindows';
 import { AluminumWindows } from './ProductsPage/AluminumWindows/AluminumWindows';
+import { BalconiesLoggias } from './ProductsPage/BalconiesLoggias/BalconiesLoggias.jsx';
 import { BrowserRouter, Routes, Router, Link, Route } from 'react-router-dom';
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
 						<Route path="/company" element={<Company />} />
 						<Route path="/pvcWindows" element={<PvcWindows />} />
 						<Route path='/AluminumWindows' element={<AluminumWindows/>}/> 
+						<Route path='/BalconiesLoggias' element={<BalconiesLoggias/>}/>
 					</Routes>
 				</BrowserRouter>
 			</div>
--- src/ProductsPage/PvcWindows/WindowSection.jsx
+++ src/ProductsPage/PvcWindows/WindowSection.jsx
@@ -1,21 +1,39 @@
 import { ReadMore } from './ReadMore';
 
-export default function WindowSection({ data }) {
+export default function WindowSection({ data, reverse = true, showReadMore = true,}) {
 	return data.map((item, index) => (
-		<div key={item.title} className={`section ${index % 2 ? 'reverse' : ''}`}>
+		<div key={item.title} className={`section ${reverse && index % 2 ? 'reverse' : ''}`}>
 			<div className="fw-text-box">
 				<h5 className="fw-special-title">{item.title}</h5>
 				<div className="fw-text">
 					<p>{item.text}</p>
-					<ReadMore width={item.width}>
-						<p>{item.hiddenText.text}</p>
-						<ul>
-							{item.hiddenText.list.map((item, index) => (
-								<li key={index}>{item}</li>
-							))}
-						</ul>
-						<p>{item.hiddenText.textTwo}</p>
-					</ReadMore>
+
+					
+					{showReadMore ? (
+						<ReadMore width={item.width}>
+							<p>{item.hiddenText.text}</p>
+
+							<ul>
+								{item.hiddenText.list.map((item, index) => (
+									<li key={index}>{item}</li>
+								))}
+							</ul>
+
+							<p>{item.hiddenText.textTwo}</p>
+						</ReadMore>
+					) : (
+						<>
+							<p>{item.hiddenText.text}</p>
+
+							<ul>
+								{item.hiddenText.list.map((item, index) => (
+									<li key={index}>{item}</li>
+								))}
+							</ul>
+
+							<p>{item.hiddenText.textTwo}</p>
+						</>
+					)}
 				</div>
 			</div>
 			<img src={item.img} alt={item.img} />
