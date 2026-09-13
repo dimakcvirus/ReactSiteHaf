5c7ccc7a04363597f9b5366f9536343e312417f3
dimakc <dimakc@mail.ru>
2026-09-13 18:19:38 +0300

    1

--- src/ForDealers/ForDealers.jsx
+++ src/ForDealers/ForDealers.jsx
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 import { Header } from '../Header/Header';
+import { Podval } from '../Podval/Podval';
+import { Galler } from '../Gallery/Galler.jsx';
 
 const ForDealersComponents = ({ className }) => {
 	return (
@@ -42,30 +44,40 @@ const ForDealersComponents = ({ className }) => {
 						<li>предоставление методических рекламных материалов</li>
 						<li>качественное гарантийное обслуживание</li>
 					</ul>
-					<p>
+					<p className="info-text">
 						Для более подробной информации о сотрудничестве отправьте заявку
 						на почту
 					</p>
-					<p>ayzatulova_la@tdhafner.ru</p>
-					<h5>Построй стабильный бизнес вместе с нами!</h5>
+					<a className="email" href="mailto:ayzatulova_la@tdhafner.ru">
+						ayzatulova_la@tdhafner.ru
+					</a>
+					<h5 className="business-title">
+						Построй стабильный бизнес вместе с нами!
+					</h5>
+					<div className="gallery">
+						<Galler />
+					</div>
 				</div>
 			</div>
+			<Podval />
 		</>
 	);
 };
 
 export const ForDealers = styled(ForDealersComponents)`
 	width: 1170px;
-	height: 834px;
-	margin: auto;
+	margin: 0 auto;
+
 	.fw-heading-center {
 		display: flex;
 		justify-content: center;
 		margin-top: 40px;
 		margin-bottom: 40px;
 	}
+
 	.fw-special-title {
-		font-family: 'Roboto Slab';
+		margin: 0;
+		font-family: 'Roboto Slab', serif;
 		font-style: normal;
 		font-weight: 400;
 		font-size: 60px;
@@ -73,18 +85,87 @@ export const ForDealers = styled(ForDealersComponents)`
 		letter-spacing: 5px;
 		color: #29293a;
 	}
+
 	.fw-text-box {
 		width: 1055px;
+		margin: 0 auto;
 	}
+
 	.strong {
-		font-weight: bold;
-		margin-bottom: 10px;
-		font-family: 'Roboto Slab';
+		margin: 0 0 10px;
+		font-family: 'Roboto Slab', serif;
 		font-style: normal;
 		font-weight: 700;
-		line-height: 28px;
 		font-size: 18px;
+		line-height: 28px;
 		letter-spacing: 2px;
 		color: #29293a;
+	}
+
+	ul {
+		margin: 0;
+		padding: 0;
+	}
+
+	li {
+		position: relative;
+		padding-left: 15px;
+		list-style: none;
+
+		font-family: 'Roboto Slab', serif;
+		font-style: normal;
+		font-weight: 400;
+		font-size: 18px;
+		line-height: 28px;
+		letter-spacing: 2px;
+		color: #29293a;
+	}
+
+	li::before {
+		content: '-';
+		position: absolute;
+		left: 0;
+	}
+
+	.info-text {
+		font-family: 'Roboto Slab', serif;
+		font-style: normal;
+		font-weight: 400;
+		font-size: 18px;
+		line-height: 28px;
+		letter-spacing: 2px;
+		color: #29293a;
+		margin: 25px 0 0;
+	}
+
+	.email {
+		display: inline-block;
+		margin-top: 0;
+
+		font-family: 'Roboto Slab', serif;
+		font-style: normal;
+		font-weight: 400;
+		font-size: 18px;
+		line-height: 28px;
+		letter-spacing: 2px;
+
+		color: #7ca82b;
+		text-decoration: none;
+	}
+	.business-title {
+		margin: 30px 0 0;
+
+		text-align: center;
+		font-family: 'Roboto Slab', serif;
+		font-style: normal;
+		font-weight: 700;
+		font-size: 18px;
+		line-height: 28px;
+		letter-spacing: 2px;
+
+		color: #29293a;
+	}
+	.gallery {
+		margin-bottom: 5%;
 	}
 `;
